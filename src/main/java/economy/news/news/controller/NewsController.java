03c981e084acc1cf571589fad0d0658c1e9eacf8
JoGyoungJun <yoop80075@gmail.com>
2024-11-28 13:10:29 +0900

    urlupdate

--- src/main/java/economy/news/news/controller/NewsController.java
+++ src/main/java/economy/news/news/controller/NewsController.java
@@ -3,14 +3,16 @@ package economy.news.news.controller;
 import economy.news.news.domain.News;
 import economy.news.news.service.NewsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
-@RequestMapping("/api/news")
+@RequestMapping("/api/v1/news")
 @RequiredArgsConstructor
 public class NewsController {
     private final NewsService newsService;
